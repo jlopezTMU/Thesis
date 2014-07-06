9fb8cc2249325d2166ab166bfb7fab4405ed38ef
Andriy Miranskyy <miranska@yahoo.com>
2014-07-06 09:17:00 -0400

    Fixing output: first value on a line should be "disctinct word count", not doc id.

--- tools/data_preparation/utils.R
+++ tools/data_preparation/utils.R
@@ -1,8 +1,8 @@
 ## The function takes term document matrix (sparse representation) and exports it
-## in the following format "docID wordId:WordCount ...", one docId per line.
-## For example, "10 15:2 21:4" means document id 10 has two words with ids 15 and 21, 
-## appearing 2 and 4 times, respectively. This format is used by LDA and HDP-LDA code
-## provided by Blei's team.
+## in the following format "distinctWordCount wordId:WordCount ...", one document per line.
+## For example, "2 15:7 21:4" means that a document contains two distinct words 
+## with ids 15 and 21, appearing 7 and 4 times, respectively. 
+## This format is used by LDA and HDP-LDA code provided by Blei's team.
 ##
 ## Usage example: export2blei(myTermDocMatrix, "~/foo.txt")
 ## Note that the existing file will be overwritten
@@ -18,7 +18,7 @@ export2blei <- function(tdm, fileName){
     # I am not certain if $j is guaranteed to be consecutive; 
     # hence the inefficient search using which
     wordIndexes <- which( tdm$j == docId)
-    cat(docId, file = fileName, sep = "", append = T)
+    cat(length(wordIndexes), file = fileName, sep = "", append = T)
     for(wordIndex in  wordIndexes){
       cat(" ", tdm$i[wordIndex], ":", tdm$v[wordIndex], file = fileName, sep = "", append = T)
     }
