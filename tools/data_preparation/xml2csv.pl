87aae2c2ec2ec4fe148a4d9e6683ad479964bbf4
Andriy Miranskyy <miranska@yahoo.com>
2014-07-23 23:06:54 -0400

    Adding extraction of top-voted answers

--- tools/data_preparation/xml2csv.pl
+++ tools/data_preparation/xml2csv.pl
@@ -13,6 +13,8 @@ use HTML::Strip;
 use utf8;
 use Text::Unidecode;    #translates unicode into ascii
 
+my $minVoteCount = 3;   #minimum number of votes required to save an answer
+
 sub transform;
 
 if ( scalar(@ARGV) != 2 ) {
@@ -26,27 +28,53 @@ my $ref = XMLin( $ARGV[0] );
 open( my $fid, ">" . $ARGV[1] )
   or die("Cannot open $ARGV[1] : $!\n");    #export to CSV
 
-#TODO: add the following attributes:
-#	Score == vote count -- stored in answer
-#	AcceptedAnswerId="4" -- stored in the question
-
 my $hs = HTML::Strip->new();
-print $fid "create_ts\ttitle\tbody\n";      #header
+print $fid "create_ts\tid\ttags\ttitle\tbody\tanswers\n";    #header
 foreach ( @{ $ref->{row} } ) {
-	if ( $_->{PostTypeId} == "1" ) {        #if the post is the original question
+	if ( $_->{PostTypeId} == "1" ) {                         #if the post is the original question
 		my $creationDate = ( $_->{CreationDate} );
-		my $title        = transform( $_->{Title} );
-		my $body         = transform( $hs->parse( $_->{Body} ) );    #strip HTML tags
-		print $fid $creationDate, "\t", $title . "\t" . $body . "\n";
+		my $id           = $_->{Id};
+		my $tags         = $_->{Tags};
+		my $bestAnswerId = $_->{AcceptedAnswerId};
+
+		#remove "<" and replace ">" with " "
+
+		$tags =~ s/></^&^/g;
+		$tags =~ s/^<//g;
+		$tags =~ s/>$//g;
+
+		my $title   = transform( $_->{Title} );
+		my $body    = transform( $hs->parse( $_->{Body} ) );                 #strip HTML tags
+		my $answers = getAnswersText( $id, $minVoteCount, $bestAnswerId );
+		print $fid join( "\t", ( $creationDate, $id, $tags, $title, $body, $answers ) ) . "\n";
 	}
 }
 close $fid;
 $hs->eof;
 
 #convert unicode to ascii and make it lower case
-sub transform{
+sub transform {
 	my ($txt) = @_;
 	$txt =~ s/\n|\t/ /g;          #replace new line or tab with space
 	$txt =~ s/[[:punct:]]/ /g;    #replace punctuation with space
 	return lc( unidecode($txt) ); #convert #convert unicode to latin and change words to lowercase
+}
+
+sub getAnswersText {
+	my ( $questionId, $minVote, $AcceptedAnswerId ) = @_;
+	my $answers = "";
+	foreach ( @{ $ref->{row} } ) {
+		if (
+			(
+				( $_->{PostTypeId}   eq "2" )            #record type = answer
+				and ( $_->{ParentId} eq $questionId )    #answer to question $questionId
+				and ( ( $_->{Score} + 0 ) >= $minVote )  #minimal vote
+			)
+			or ( $_->{Id} eq $AcceptedAnswerId )         #always get accepted answer
+		  )
+		{
+			$answers .= transform( $hs->parse( $_->{Body} ) ) . " ";
+		}
+	}
+	return $answers;
 }
