f75e8f42096f7bdf464a5184303749b30c17c50f
Andriy Miranskyy <miranska@yahoo.com>
2014-08-07 19:11:37 -0400

    Adding a script reading and cleaning text corpus, converting dtm to data frame, and saving it to a file

---
+++ tools/data_preparation/dtm2dataframe.R
@@ -0,0 +1,25 @@
+library(tm)
+library(topicmodels)
+library(foreach)
+library(doParallel)
+
+source("utils.R")
+
+args <- commandArgs(trailingOnly = TRUE)
+readFrom <- args[1]
+saveTo <-  args[2]
+
+corp <- createCorp(readFrom, 2012, 2)
+
+## Build a Document-Term Matrix
+dtm <- ç(corp, control = list(minWordLength = 2)) #keep words of lenght 2 or longer
+cat("Before tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+dtm <- removeFrequentWords(dtm) #removing based on median tf-idf value
+cat("After tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+
+
+dtm.data_frame <- as.data.frame(as.matrix(dtm))
+
+save(dtm.data_frame, file = saveTo)
+
+
