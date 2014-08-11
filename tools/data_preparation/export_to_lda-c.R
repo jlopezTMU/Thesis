4cfdd1902baa0dd7529e01a009d88f1309f97ec6
Andriy Miranskyy <miranska@yahoo.com>
2014-08-11 11:04:33 -0400

    updating export to lda-c, adding year and month as external argument

--- tools/data_preparation/export_to_lda-c.R
+++ tools/data_preparation/export_to_lda-c.R
@@ -1,54 +1,27 @@
-## The script reads posts in delimeted format, massages them,
-## and saves final text corpus in LDA-C format.
-## Usage: Rscript export_to_lda-c.R in_file_name out_file_name
+## The script reads posts in delimited format, massages them,
+## and saves final text corpus in LDA-C format, 
+## appending ".year-month.lda-c" to in_file_name
+## Usage: Rscript export_to_lda-c.R in_file_name year month
 
-## Install and load packages
 library(tm)
-library(topicmodels)
 
-source("utils.R") # export2lda_c
+source("utils.R")
 
 args <- commandArgs(trailingOnly = TRUE)
-
-###############################################################################
-# Setup Begin
-###############################################################################
-#read data from
-#readFrom <- "/Users/miranska/Documents/workspace/se_topics/sample.xml.csv"
-#readFrom <- "~/Downloads/android.stackexchange.com/Posts.xml.csv"
 readFrom <- args[1]
+year  <-  as.integer(args[2])
+month <-  as.integer(args[3])
 
-#save data on LDA-C format to
-#saveTo   <- "/Users/miranska/Documents/workspace/se_topics/sample.xml.csv.lda-c" 
-#saveTo   <- "~/Downloads/android.stackexchange.com/Posts.xml.csv.lda-c" 
-saveTo    <- args[2]
-#mininmal lenght of a word in the dictionary
-minWordLenghtToKeep <- 4
-###############################################################################
-# Setup End
-###############################################################################
+saveTo <-  paste(readFrom, ".", year, "-", month, ".lda-c", sep ="")
 
-## Load the data
-#  Separator is set to \n to force 1 column per row. DataframeSource gets confused otherwise.
-Posts <- read.delim(file = readFrom, header = T, quote = "", sep = "\n")
-
-cat("Read", nrow(Posts), "rows from", readFrom, "\n")
-## Build a corpus
-corp <- Corpus(DataframeSource(data.frame(Posts)))
-
-## Transform data
-## TODO: it seems that tm_map parallizes execution via duplication of R instances, 
-##   consuming lots of memory. If needed, we can use the functions below (e.g., stripWhitespace)
-##   directly.
-corp = tm_map(corp, stripWhitespace) # Eliminate whitespace char
-corp = tm_map(corp, removePunctuation) # Remove punctuation
-corp = tm_map(corp, removeNumbers) # Eliminate numbers
-corp = tm_map(corp, removeWords, stopwords('english')) # Remove Stopwords
-corp = tm_map(corp, stemDocument) # Stemming
-
+## create text corpus
+corp <- createCorp(readFrom, year, month)
 ## Build a Document-Term Matrix
-dtm <- DocumentTermMatrix(corp, control = list(minWordLength = minWordLenghtToKeep))
-cat ("Created dtm for", length(unique(dtm$i)), "documents with", length(dtm$i), "elements\n")
+dtm <- DocumentTermMatrix(corp, control = list(minWordLength = 2)) #keep words of length 2 or longer
+cat("Before tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+dtm <- removeFrequentWords(dtm) #removing based on median tf-idf value
+cat("After tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+
 
 ## Export text corpus to LDA-C format
 export2lda_c(dtm, saveTo)
