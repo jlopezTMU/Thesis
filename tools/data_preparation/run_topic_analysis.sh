919aa676405021bc782162602e3a88f9f439ce34
Andriy Miranskyy <miranska@yahoo.com>
2015-10-30 17:48:47 -0400

    Adding diag log messages to harnesses

--- tools/data_preparation/run_topic_analysis.sh
+++ tools/data_preparation/run_topic_analysis.sh
@@ -4,6 +4,8 @@ for YEAR in 2012 2013 2014
 do
 	for MONTH in {1..12} 
 	do
+		echo "Running script for $YEAR-$MONTH"
 		 Rscript do_lda_analysis_topics.R /media/data/text/topic_analysis/Posts.xml.w_ans.csv $YEAR $MONTH
+		echo "Done with $YEAR-$MONTH"
 	done
 done
--- tools/data_preparation/run_topic_analysis_quarterly.sh
+++ tools/data_preparation/run_topic_analysis_quarterly.sh
@@ -4,6 +4,8 @@ for YEAR in 2012 2013 2014
 do
 	for QUARTER in {1..4} 
 	do
+		echo "Running script for $YEAR-$MONTH"
 		 Rscript do_lda_analysis_topics_quarterly.R ../../../topic_analysis/Posts.xml.w_ans.csv $YEAR $QUARTER
+		echo "Done with $YEAR-$MONTH"
 	done
 done
