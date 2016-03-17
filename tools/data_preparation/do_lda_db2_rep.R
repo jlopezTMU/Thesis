b76e262f0d2949f0bf335d18297807520835875d
George Lopez <RUMCS@users.noreply.github.com>
2016-03-17 12:23:44 -0400

    do_lda_db2_rep.R
    This program generates the probabilities per keyword per topic, it also produces a report file with the questions related to each topic. In this case we are extracting 5 topics and 20 keywords per topic.

---
+++ tools/data_preparation/do_lda_db2_rep.R
@@ -0,0 +1,99 @@
+## The script reads posts in delimited format, and generates a list of keyword
+## Usage: Rscript my_Script_name.R out_directory/ in_file_name yyyy
+## This programs includes the probability per keyword
+## It also generates a report that includes the URL of the question made by topic,
+## the output file is then treated in Excel for calculating totals 
+
+library(tm)
+library(foreach)
+library(doParallel)
+
+source("utilsdb2.R")
+xURL <- "http://dba.stackexchange.com/questions/"
+############################### Comment the following lines  to run in PC ##############################
+args <- commandArgs(trailingOnly = TRUE)
+outDir <- args[1]
+readFrom <- paste(args[1], args[2],sep="")
+year  <-  as.integer(args[3])
+
+################################# Uncomment the following lines to run in PC ###########################
+## readFrom <- "db2.xml.csv"
+## year <- 2015
+########################################################################################################
+## month <-  as.integer(args[3])
+
+topKeywordCount <- 20 ## SET TO 20 FOR THIS RUN
+
+## corp <- createCorp(readFrom) ## replaced by:
+corp <- createCorp(readFrom, year) ## added year
+
+# To access original id of a document in VCorp run corp[[ index_of_document_1_to_N ]]$meta$id
+# To access original id of a document in DocumentTermMatrix run dtm$dimnames$Docs[ index_of_document_1_to_N ]
+
+cat("DEBUG after calling createCorp!\n")
+## Build a Document-Term Matrix
+dtm <- DocumentTermMatrix(corp, control = list(minWordLength = 2)) #keep words of lenght 2 or longer
+cat("Before tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+#dtm <- removeFrequentWords(dtm) #removing based on median tf-idf value
+cat("After tf-idf: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+## the following line commented out to avoid sparse error
+#dtm <- removeSparseTerms(dtm, 1 - (1.1/nrow(dtm)) )  #remove terms appearing only in 1 document
+#dtm <- dtm[row_sums(dtm) > 0,]  #remove docs that have no terms remaining (unlikely event)
+cat("After removing terms appearing only in 1 document: term count =", ncol(dtm), ", doc count =", nrow(dtm), "\n")
+
+
+#setup parallel backend to use 8 processors
+cl<-makeCluster(8) ## change to 8 for multiprocessing
+registerDoParallel(cl)
+
+## original foreach(topicCount = 2:nrow(dtm) #max = 1 topic per document
+##foreach(topicCount = c(2, 3) #max = 1 topic per document
+foreach(topicCount = c(5) #max = 1 topic per document ## changed topic count=5
+        , .packages='topicmodels' #include package
+) %do%
+{ #change to %dopar% for parallel execution
+  cat("*RUNNING SINGLE PROCESSING*\n")
+  cat("TopicCount:", topicCount, "\n") #to screen (no screen output is parallel mode)  
+  mdl <- LDA(dtm, topicCount) #LDA model 
+
+  topic.keyword <- terms(mdl, topKeywordCount)
+  mdl.post <- posterior(mdl) #get posterior data
+}
+
+## the following nested loop was added to append the p-value per topic.keyword
+for (i in 1:topKeywordCount){
+  for (j in 1:topicCount) {
+    topic.keyword[i,j] <- paste(topic.keyword[i,j], "\t/", mdl.post$terms[[j, topic.keyword[i,j]]])
+    ## cat("This is the P-Value for  term:", i, ":", j, ">", topic.keyword[i,j], "\n")
+  }
+}
+
+cat("DEBUG: year is", year, "\n")
+#################################### Comment for running in PC ######################################
+saveTo <- paste(args[1],"report-", args[2], "-", year, ".txt", sep = "")
+#################################### Uncomment for running in PC ####################################
+##saveTo <- paste("report-", readFrom, "-", year, ".txt")
+#####################################################################################################
+
+################################### Comment for running in PC ########################################################
+write.csv(topic.keyword, file = paste(outDir,"lda_keywords_", args[2],"_",year, "-", topicCount, ".csv", sep = "") )
+################################### Uncomment for running in PC ######################################################
+## write.csv(topic.keyword, file = paste("TEST_", readFrom, "_", year, "-", topicCount, ".csv", sep = "") )
+
+## code added to support report creation
+
+## Will retrieve all ids and questions
+t <- topics(mdl)
+l <- length(topics(mdl))
+saveTo <- paste(args[1], "report-", args[2], "-", year, ".txt") ## path to write the report file
+cat("question_id\tquestion_description\ttopic\t\n", sep="\t", append = T, file = saveTo) # to file
+for (i in 1:l){
+     ri <- corp[[i]]$meta$id
+     rq <- corp[[i]]$meta$quest
+     rt <- t[i]
+     r <- paste(xURL, ri, "\t", rq, "\t", rt, sep="")
+     ## cat ("@ for cat i=", i, "\n")
+     cat(r, "\n", append = T, file = saveTo) # to file
+}
+
+cat("Done\n")
