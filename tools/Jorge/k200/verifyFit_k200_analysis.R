d37ddf0cf0a11fc22f5fa16f202d63a18fd47960
Andriy Miranskyy <miranska@yahoo.com>
2017-06-24 09:34:21 -0400

    Adding additional performance graphs

--- tools/Jorge/k200/verifyFit_k200_analysis.R
+++ tools/Jorge/k200/verifyFit_k200_analysis.R
@@ -5,12 +5,19 @@ readFromfileName = "topX.csv.original.zip"
 
 if(data_filter_200){
   model_file_name <- "./models/models_fitting.remove_top_25_percent_and_values_gt_200.rda"
-  models_performance_file_name <- "./models/models_performance.remove_top_25_percent_and_values_gt_200.csv"
-  boxplot_file_name <- "./models/models_performance.remove_top_25_percent_and_values_gt_200.pdf"
+  models_performance_file_name <- "./models/models_performance__remove_top_25_percent_and_values_gt_200.csv"
+  rmse_per_model_file_name <- "./models/models_performance__remove_top_25_percent_and_values_gt_200.pdf"
+  rmse_per_dataset_file_name <- "./models/models_performance__per_dataset__remove_top_25_percent_and_values_gt_200.pdf"
+  rmse_per_x_file_name <- "./models/models_performance__per_x__remove_top_25_percent_and_values_gt_200.pdf"
+  rmse_per_n_file_name <- "./models/models_performance__per_n__remove_top_25_percent_and_values_gt_200.pdf"
 }else{
   model_file_name <- "./models/models_fitting.remove_top_25_percent.rda"
   models_performance_file_name <- "./models/models_performance.remove_top_25_percent.csv"
-  boxplot_file_name <- "./models/models_performance.remove_top_25_percent.pdf"
+  rmse_per_model_file_name <- "./models/models_performance__remove_top_25_percent.pdf"
+  rmse_per_dataset_file_name <- "./models/models_performance__per_dataset.remove_top_25_percent.pdf"
+  rmse_per_x_file_name <- "./models/models_performance__per_x__remove_top_25_percent.pdf"
+  rmse_per_n_file_name <- "./models/models_performance__per_n__remove_top_25_percent.pdf"
+  
 }
 
 
@@ -23,7 +30,7 @@ dat.stats.long <- gather(dat.stats, model_name, rmse, rmse_flex_model_simple:rms
 #reorder for box-plot
 dat.stats.long$model_name <- factor(dat.stats.long$model_name, levels=c("rmse_flex_model_simple", "rmse_flex_model_complex", "rmse_constraint_model_simple", "rmse_constraint_model_complex",  "rmse_tailored_flex_lm", "rmse_tailored_constr_lm" ))
 
-pdf(boxplot_file_name)
+pdf(rmse_per_model_file_name)
 boxplot(rmse ~ model_name, 
         data = dat.stats.long,
         log = "y",
@@ -36,6 +43,62 @@ boxplot(rmse ~ model_name,
 grid()
 dev.off()
 
+pdf(rmse_per_dataset_file_name)
+dat.stats$ds_merged_name <- as.factor(paste(dat.stats$dataset_name, "-", dat.stats$dt_partition))
+boxplot(rmse_constraint_model_complex ~ ds_merged_name, 
+        data = dat.stats,
+        log = "y",
+        #xlab = "Model Name",
+        las = 2,
+        ylab = "RMSE",
+        par(mar = c(12, 5, 4, 2)+ 0.1)
+        #names = c("Flex - simple", "Flex - complex", "Constr. - simple", "Constr. - complex", "Ind. fit - flex", "Ind. fit - constr")
+)
+grid()
+dev.off()
+
+pdf(rmse_per_dataset_file_name)
+dat.stats$ds_merged_name <- as.factor(paste(dat.stats$dataset_name, "-", dat.stats$dt_partition))
+boxplot(rmse_constraint_model_complex ~ ds_merged_name, 
+        data = dat.stats,
+        log = "y",
+        #xlab = "Model Name",
+        main = "Model - constrained, complex",
+        las = 2,
+        ylab = "RMSE",
+        par(mar = c(12, 5, 4, 2)+ 0.1)
+        #names = c("Flex - simple", "Flex - complex", "Constr. - simple", "Constr. - complex", "Ind. fit - flex", "Ind. fit - constr")
+)
+grid()
+dev.off()
+
+pdf(rmse_per_x_file_name)
+boxplot(rmse_constraint_model_complex ~ X, 
+        data = dat.stats,
+        log = "y",
+        xlab = "X",
+        main = "Model - constrained, complex",
+        #las = 2,
+        ylab = "RMSE"
+        #par(mar = c(12, 5, 4, 2)+ 0.1)
+        #names = c("Flex - simple", "Flex - complex", "Constr. - simple", "Constr. - complex", "Ind. fit - flex", "Ind. fit - constr")
+)
+grid()
+dev.off()
+
+pdf(rmse_per_n_file_name)
+boxplot(rmse_constraint_model_complex ~ N, 
+        data = dat.stats,
+        log = "y",
+        xlab = "N",
+        main = "Model - constrained, complex",
+        #las = 2,
+        ylab = "RMSE"
+        #par(mar = c(12, 5, 4, 2)+ 0.1)
+        #names = c("Flex - simple", "Flex - complex", "Constr. - simple", "Constr. - complex", "Ind. fit - flex", "Ind. fit - constr")
+)
+grid()
+dev.off()
 
 
 #summary stats
